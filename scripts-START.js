f5b85f359eb0e4dec05fc54123b893834183f518
charles mancuso <charlesmancuso66@yahoo.com>
2020-05-06 21:48:18 -0700

    Finish display end time function

--- scripts-START.js
+++ scripts-START.js
@@ -6,6 +6,7 @@ function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -29,5 +30,5 @@ function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
   const minutes = end.getMinutes();
-  endTime.textContent = `Be Back At ${hour}:${minutes}`
+  endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes}`
 }
