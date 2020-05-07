6b3a9752c6c785d4f6fbb8f8b1bdf7f7d0498af2
charles mancuso <charlesmancuso66@yahoo.com>
2020-05-06 21:46:21 -0700

    Add display end time function to show when we will be back

--- scripts-START.js
+++ scripts-START.js
@@ -1,5 +1,6 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
   const now = Date.now();
@@ -19,6 +20,14 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  const display = `${minutes}:${remainderSeconds}`;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  document.title = display;
   timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hour}:${minutes}`
 }
